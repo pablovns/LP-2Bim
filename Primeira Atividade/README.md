2906118e730973084cea930cfac40d98c436dc72
pablovns <viniciuspablo962@gmail.com>
2023-10-26 20:35:39 -0300

    Initial Commit

---
+++ Primeira Atividade/README.md
@@ -0,0 +1,15 @@
+- Desenvolva uma aplicação da qual seja possível efetuar os seguintes cálculos: Soma,
+Multiplicação, Divisão e Subtração. É obrigatória a utilização de funções conforme
+solicitado logo abaixo, na qual o usuário vai informar sempre a opções que deseja
+executar na calculadora, ONDE:
+    - 1.1. (Peso 2,0) – A operação de soma deve ser efetuada em uma função no JS,
+onde os valores são informados pelo usuário e retornado a soma dos dois;
+    - 1.2. (Peso 2,0) – A operação de subtração deve ser efetuada em um função no
+JS;
+    - 1.3. (Peso 2,0) – A operação de divisão deve-se usar função no JS;
+    - 1.4. (Peso 2,0) – A operação de multiplicação deve usar função no JS.
+    - 1.5. (Peso 2,0) – Se quiser usar menu ou botões para que seja possível
+escolher as operações e junto nela deve existir a possibilidade de escolher a
+opção Sair da página atual ou encerrar a execução da aplicação, pois será
+possível começar os cálculos após os resultados;
+Obs: tratar divisão por zero;
---
+++ Primeira Atividade/index.js
@@ -0,0 +1,69 @@
+const selectBoxOperacao = document.getElementById("selectBoxOperacao");
+const botaoCalcular = document.getElementById("botaoCalcular");
+const labelResultado = document.getElementById("labelResultado");
+const form = document.getElementById("form");
+const pResultado = document.getElementById("pResultado");
+const inputNumeroA = document.getElementById("numeroA");
+const inputNumeroB = document.getElementById("numeroB");
+
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function divisao(a, b) {
+    if (b == 0) {
+        alert("Não é possível dividir por zero. Informe outro número ou mude a operação.");
+        return;
+    }
+    return a / b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function esconderResultado() {
+    pResultado.classList.add("hidden");
+}
+
+function mostrarResultado() {
+    pResultado.classList.remove("hidden");
+}
+
+function limparCampos() {
+    selectBoxOperacao.selectedIndex = 0;
+    inputNumeroA.value = "";
+    inputNumeroB.value = "";
+    esconderResultado();
+}
+
+const objOperacoes = {
+    "+": (a, b) => soma(a, b),
+    "-": (a, b) => subtracao(a, b),
+    "/": (a, b) => divisao(a, b),
+    "*": (a, b) => multiplicacao(a, b)
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    esconderResultado();
+
+    const operacao = selectBoxOperacao.value;
+    let numeroA = Number(inputNumeroA.value);
+    let numeroB = Number(inputNumeroB.value);
+
+    if (operacao == "") {
+        alert("Selecione uma operação.");
+        return;
+    }
+
+    let resultado = objOperacoes[operacao](numeroA, numeroB);
+    if (resultado != undefined) {
+        labelResultado.innerText = resultado;
+        mostrarResultado();
+    }
+})
