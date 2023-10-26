39f129e5d01d9f4ba38bd06d1d690f32763b3c9f
pablovns <viniciuspablo962@gmail.com>
2023-10-26 20:42:30 -0300

    Formatação

--- index.js
+++ index.js
@@ -7,63 +7,65 @@ const inputNumeroA = document.getElementById("numeroA");
 const inputNumeroB = document.getElementById("numeroB");
 
 function soma(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtracao(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function divisao(a, b) {
-    if (b == 0) {
-        alert("Não é possível dividir por zero. Informe outro número ou mude a operação.");
-        return;
-    }
-    return a / b;
+  if (b == 0) {
+    alert(
+      "Não é possível dividir por zero. Informe outro número ou mude a operação."
+    );
+    return;
+  }
+  return a / b;
 }
 
 function multiplicacao(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function esconderResultado() {
-    pResultado.classList.add("hidden");
+  pResultado.classList.add("hidden");
 }
 
 function mostrarResultado() {
-    pResultado.classList.remove("hidden");
+  pResultado.classList.remove("hidden");
 }
 
 function limparCampos() {
-    selectBoxOperacao.selectedIndex = 0;
-    inputNumeroA.value = "";
-    inputNumeroB.value = "";
-    esconderResultado();
+  selectBoxOperacao.selectedIndex = 0;
+  inputNumeroA.value = "";
+  inputNumeroB.value = "";
+  esconderResultado();
 }
 
 const objOperacoes = {
-    "+": (a, b) => soma(a, b),
-    "-": (a, b) => subtracao(a, b),
-    "/": (a, b) => divisao(a, b),
-    "*": (a, b) => multiplicacao(a, b)
-}
+  "+": (a, b) => soma(a, b),
+  "-": (a, b) => subtracao(a, b),
+  "/": (a, b) => divisao(a, b),
+  "*": (a, b) => multiplicacao(a, b),
+};
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    esconderResultado();
+  e.preventDefault();
+  esconderResultado();
 
-    const operacao = selectBoxOperacao.value;
-    let numeroA = Number(inputNumeroA.value);
-    let numeroB = Number(inputNumeroB.value);
+  const operacao = selectBoxOperacao.value;
+  let numeroA = Number(inputNumeroA.value);
+  let numeroB = Number(inputNumeroB.value);
 
-    if (operacao == "") {
-        alert("Selecione uma operação.");
-        return;
-    }
+  if (operacao == "") {
+    alert("Selecione uma operação.");
+    return;
+  }
 
-    let resultado = objOperacoes[operacao](numeroA, numeroB);
-    if (resultado != undefined) {
-        labelResultado.innerText = resultado;
-        mostrarResultado();
-    }
-})
+  let resultado = objOperacoes[operacao](numeroA, numeroB);
+  if (resultado != undefined) {
+    labelResultado.innerText = resultado;
+    mostrarResultado();
+  }
+});
